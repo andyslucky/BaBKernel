22da97c7705c902d86d2d45f65e12ee21e40b75e
Jonathan Cooper <drowsysaturn@gmail.com>
2019-10-14 22:38:37 -0400

    Added malloc and free work around

--- memory/memory.c
+++ memory/memory.c
@@ -1,12 +1,122 @@
-#include "kmem.h"
-#define HEAPSTART (void*)0x0
+#include "memory.h"
 
-void* memoryMarker = HEAPSTART;
+#define HEAPSTART (void*)0xFF
+#define HEAPMAX 10000
+#define MAX_ALLOCS 1024
 
-void* malloc(unsigned int size)
+typedef struct {
+    void *start;
+    size_t size;
+    char freed;
+} AddressRecord;
+
+AddressRecord records[MAX_ALLOCS];
+size_t used_records = 0;
+
+void memzero(void *dest, size_t count)
 {
-    MemBlock start = createBlock(memoryMarker,size);
-    void* end = start.end;
-    memoryMarker = ++end;
-    return start.start;
+    char *destCp = (char*)dest;
+    for (size_t i = 0; i < count; i++)
+    {
+        destCp[i] = (char)0;
+    }
+}
+
+void memcpy(void *dest, void *src, size_t count)
+{
+    char *destCp = (char*)dest;
+    char *srcCp = (char*)src;
+    for (size_t i = 0; i < count; i++)
+    {
+        destCp[i] = srcCp[i];
+    }
+}
+
+int mem_inuse(size_t addr)
+{
+    for (size_t i = 0; i < used_records; i++)
+    {
+        AddressRecord *record = &records[i];
+        if (record->freed)
+        {
+            continue;
+        }
+        if (record->start <= addr && record->start + record->size > addr)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void* find_mem_space(size_t size)
+{
+    for (size_t i = HEAPSTART; i < HEAPMAX; i++)
+    {
+        if (i + size >= HEAPMAX)
+        {
+            return (void*)0;
+        }
+        if (!mem_inuse(i))
+        {
+            int has_enough = 1;
+            for (size_t j = i; j < size; j++)
+            {
+                if (mem_inuse(j))
+                {
+                    has_enough = 0;
+                    break;
+                }
+            }
+            if (has_enough)
+            {
+                return (void*)i;
+            }
+        }
+    } 
+}
+
+void add_mem_record(void* start, size_t size)
+{
+    AddressRecord record;
+    record.size = size;
+    record.start = start;
+    record.freed = 0;
+    for (size_t i = 0; i < used_records; i++)
+    {
+        if (records[i].freed)
+        {
+            records[i] = record;
+            return;
+        }
+    }
+    if (used_records + 1 >= MAX_ALLOCS)
+    {
+        // TODO: Segfault
+    }
+    records[used_records] = record;
+    used_records++;
+}
+
+void* malloc(size_t size)
+{
+    void* start = find_mem_space(size);
+    if (start == (void*)0)
+    {
+        // TODO: Segfault
+    }
+    add_mem_record(start, size);
+}
+
+void free(void* addr)
+{
+    for (size_t i = 0; i < used_records; i++)
+    {
+        if (records[i].start == addr)
+        {
+            records[i].freed = 1;
+            return;
+        }
+    }
+    // TODO: Segfault
 }
--- memory/memory.h
+++ memory/memory.h
@@ -1,1 +1,13 @@
+#ifndef memory_h
+#define memory_h
+
+#ifndef size_t
+    #define size_t unsigned long long
+#endif
+
 void* malloc(unsigned int size);
+void free(void *data);
+void memcpy(void *dest, void *src, size_t count);
+void memzero(void *dest, size_t count);
+
+#endif
